57d94d8d25825b8644af49ee7588c945c139093f
mamabilou <clergerbiloudia5@gmail.com>
2025-06-28 09:24:17 -0700

    Update mama.js

--- mama.js
+++ mama.js
@@ -1,15 +1,14 @@
- function updateClockAndDate() {
+
+    function updateClockAndDate() {
       const now = new Date();
+      const optionsDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+      const date = now.toLocaleDateString('fr-FR', optionsDate);
+      const time = now.toLocaleTimeString('fr-FR');
 
-      const hours = String(now.getHours()).padStart(2, '0');
-      const minutes = String(now.getMinutes()).padStart(2, '0');
-      const seconds = String(now.getSeconds()).padStart(2, '0');
-      document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
-
-      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-      const dateStr = now.toLocaleDateString('fr-FR', options);
-      document.getElementById("date").textContent = dateStr;
+      document.getElementById('clock').textContent = ` Heure actuelle : ${time}`;
+      document.getElementById('date').textContent = ` Aujourd’hui : ${date}`;
     }
 
-    setInterval(updateClockAndDate, 1000);
     updateClockAndDate();
+    setInterval(updateClockAndDate, 1000);
+ 
